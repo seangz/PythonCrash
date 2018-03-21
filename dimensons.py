228f25ee94b028549d497a5538083466a196c2d1
seangz <sean.gnanarajah@gmail.com>
2018-03-21 12:21:25 -0400

    update

---
+++ dimensons.py
@@ -0,0 +1,23 @@
+dimensions = (200,50)
+for dimension in dimensions:
+	print(dimension)
+
+print(dimensions[0])
+print(dimensions[1])
+
+dimensions = (100,200,300)
+for dimension in dimensions:
+	print(dimension)
+
+print("\nThis restaurant only serves these foods")
+
+foods = ('hotdog','pizza','hamburger','french fries','soda')
+
+for food in foods:
+	print(food.title())
+
+print("\nThis is their dessert menu")
+foods = ('cake','cupcake','cookie','brownie','ice cream')
+
+for food in foods:
+	print(food.title())
---
+++ players.py
@@ -0,0 +1,9 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+
+print("Here are the first three players")
+
+for player in players[:3]:
+	print(player.title())
+
+	
