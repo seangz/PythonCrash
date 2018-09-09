5ca132270be588e1ae4abb0eccc6736a5943ab4f
Sean <sean.gnanarajah@gmail.com>
2018-09-09 00:43:43 -0400

    commit

--- Classes/electric_car.py
+++ Classes/electric_car.py
@@ -40,7 +40,20 @@ class Battery():
 
     def describe_battery(self):
         """Print battery size"""
-        print(self.battery_size)
+        print("This car has a " + str(self.battery_size)) + "-kWh battery."
+
+    def get_range(self):
+        """Print a statement about the range the battery provides"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge"
+
+        print(message)
+
 
 
 class ElectricCar(Car):
@@ -60,3 +73,4 @@ my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
 
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
